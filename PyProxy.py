e061ee17848a7b89096f97264a2661c885100104
Leo Liu <leo.qing.liu@gmail.com>
2019-02-13 17:51:21 +0800

    解析收到的http请求

--- PyProxy.py
+++ PyProxy.py
@@ -1,4 +1,51 @@
-import socket
+import socket, re
+
+
+g_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 ' \
+          'Safari/537.36'
+
+
+def get_http_body(sock, length):
+    block_size = 2048
+    data = bytes()
+    received = 0
+    total = int(length)
+
+    while received < total:
+        part = sock.recv(block_size)
+        data += part
+        received += len(part)
+
+    ret_str = str(data, encoding="utf-8")
+    return ret_str
+
+
+def receive_http_response_body(sock, lines):
+    body_size = get_content_length(lines)
+    body = get_http_body(sock, body_size)
+    return body
+
+
+def get_content_length(lines):
+    for line in lines:
+        if line.startswith('Content-Length'):
+            pattern = "[0-9]*$"
+            regex = re.compile(pattern)
+            result = re.search(regex, line)
+            return result.group()
+
+
+def get_response_headers(sock):
+    lines = []
+
+    while True:
+        line = read_line(sock)
+
+        if line == '\r\n' or line == '':
+            break
+
+        lines.append(line)
+    return lines
 
 
 def read_line(sock):
@@ -26,7 +73,9 @@ class HttpProxy(object):
 
     def handle_request(self, client):
         request_line = read_line(client)
+        headers = get_response_headers(client)
         print(request_line)
+        print(headers)
 
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
